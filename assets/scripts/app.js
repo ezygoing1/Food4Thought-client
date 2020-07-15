032bb26e5e02d3fe95ca7fa2bfde94a89ffb6bb6
frederickwoodhouse <frederickwoodhouse@gmail.com>
2020-07-14 21:33:37 -0500

    Fixed app.js Fixed app.js created folders for review (events,UI,api.js)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,21 @@
 'use strict'
+
+const foodTruckEvents = require('./foodTruck/events.js')
+const userEvents = require('./user/events.js')
 const authEvents = require('/Users/frederickwoodhouse/sei/projects/Food4Thought-client/assets/scripts/auth/events.js')
 $(() => {
   authEvents.addHandlers()
+  // foodTrucks listeners:
+  $('#foodTruck-index').on('click', foodTruckEvents.onFoodTrucksIndex)
+  $('#foodTruck-show').on('submit', foodTruckEvents.onFoodTrucksShow)
+  $('#foodTruck-delete').on('submit', foodTruckEvents.onFoodTrucksDelete)
+  $('#foodTruck-update').on('submit', foodTruckEvents.onFoodTrucksUpdate)
+  $('#foodTruck-create').on('submit', foodTruckEvents.onFoodTrucksCreate)
+
+  // users listeners:
+  $('#users-index').on('click', userEvents.onUsersIndex)
+  $('#users-show').on('submit', userEvents.onUsersShow)
+  $('#users-delete').on('submit', userEvents.onUsersDelete)
+  $('#users-update').on('submit', userEvents.onUsersUpdate)
+  $('#users-create').on('submit', userEvents.onUsersCreate)
 })
